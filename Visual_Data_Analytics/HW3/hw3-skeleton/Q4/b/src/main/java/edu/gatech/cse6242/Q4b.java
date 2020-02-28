43030924a565b33aa7d6a6280504b8f3a3183245
Mooseburger1 <mooseburger1@msn.com>
2020-02-28 00:24:31 -0600

    updated

---
+++ Visual_Data_Analytics/HW3/hw3-skeleton/Q4/b/src/main/java/edu/gatech/cse6242/Q4b.java
@@ -0,0 +1,27 @@
+package edu.gatech.cse6242;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.util.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import java.io.IOException;
+
+public class Q4b {
+
+  public static void main(String[] args) throws Exception {
+    /* TODO: Update variable below with your gtid */
+    final String gtid = "gburdell3";
+
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "Q4b");
+
+    /* TODO: Needs to be implemented */
+
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
