cd0da0e4a22340194efb1054f05e8be3d604533c
Mooseburger1 <mooseburger1@msn.com>
2020-03-20 00:27:04 -0500

    hw4 vda

---
+++ Visual_Data_Analytics/HW4/hw4-skeleton/Q2/decision_tree.py
@@ -0,0 +1,37 @@
+import numpy as np 
+import ast
+
+class DecisionTree(object):
+    def __init__(self, max_depth):
+        # Initializing the tree as an empty dictionary or list, as preferred
+        self.tree = {}
+        self.max_depth = max_depth
+        pass
+    	
+    def learn(self, X, y, par_node = {}, depth=0):
+        # TODO: Train the decision tree (self.tree) using the the sample X and labels y
+        # You will have to make use of the functions in utils.py to train the tree
+
+        # Use the function best_split in util.py to get the best split and 
+        # data corresponding to left and right child nodes
+        
+        # One possible way of implementing the tree:
+        #    Each node in self.tree could be in the form of a dictionary:
+        #       https://docs.python.org/2/library/stdtypes.html#mapping-types-dict
+        #    For example, a non-leaf node with two children can have a 'left' key and  a 
+        #    'right' key. You can add more keys which might help in classification
+        #    (eg. split attribute and split value)
+        ### Implement your code here
+        #############################################
+        
+        pass
+        #############################################
+
+
+    def classify(self, record):
+        # TODO: classify the record using self.tree and return the predicted label
+        ### Implement your code here
+        #############################################
+        
+        pass
+        #############################################
